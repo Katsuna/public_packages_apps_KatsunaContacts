7b7fbc91ae638e673923c102475c13159bd6ac8c
KTakaf <ktakaf@gmail.com>
2016-01-07 14:08:12 +0200

    fab buttons

--- app/src/main/java/gr/crystalogic/oldmen/ui/activities/MainActivity.java
+++ app/src/main/java/gr/crystalogic/oldmen/ui/activities/MainActivity.java
@@ -1,22 +1,19 @@
 package gr.crystalogic.oldmen.ui.activities;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.LinearLayout;
+import android.view.View;
 
 import gr.crystalogic.oldmen.R;
 import gr.crystalogic.oldmen.ui.adapters.models.ContactListItemModel;
-import gr.crystalogic.oldmen.ui.fragments.ActionsFragment;
 import gr.crystalogic.oldmen.ui.fragments.ContactsFragment;
 import gr.crystalogic.oldmen.ui.listeners.IActionsFragmentInteractionListener;
 import gr.crystalogic.oldmen.ui.listeners.IContactsFragmentInteractionListener;
-import gr.crystalogic.oldmen.utils.Constants;
 
 public class MainActivity extends AppCompatActivity implements IContactsFragmentInteractionListener, IActionsFragmentInteractionListener {
 
@@ -25,32 +22,38 @@ public class MainActivity extends AppCompatActivity implements IContactsFragment
 
     private Step currentStep = Step.START;
     private ContactsFragment contactsFragment;
+    private FloatingActionButton searchFab;
+    private FloatingActionButton newContactFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addActionsFragment();
         contactsFragment = (ContactsFragment) getSupportFragmentManager().findFragmentById(R.id.contacts_fragment);
+        setupFabs();
     }
 
-    private void addActionsFragment() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    private void setupFabs() {
+        searchFab = (FloatingActionButton) findViewById(R.id.search_fab);
+        searchFab.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.indigo_blue)));
+        searchFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        Fragment actionsFragment = new ActionsFragment();
+            }
+        });
 
-        fragmentTransaction.replace(R.id.bottom_container, actionsFragment);
+        newContactFab = (FloatingActionButton) findViewById(R.id.new_contact_fab);
+        newContactFab.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.pink)));
 
-        // Commit the transaction
-        fragmentTransaction.commit();
-    }
-
-    private void setFragmentWeight(int id, float weight) {
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0);
-        params.weight = weight;
-        findViewById(id).setLayoutParams(params);
+        newContactFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, EditContactActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -70,30 +73,11 @@ public class MainActivity extends AppCompatActivity implements IContactsFragment
         }
     }
 
-    private final Handler handler = new Handler();
-    private final Runnable runnable = new Runnable() {
-        public void run() {
-            setActionsVisible(true);
-        }
-    };
-
-    private void setActionsVisible(boolean show) {
-        if (show) {
-            setFragmentWeight(R.id.bottom_container, 2f);
-        } else {
-            setFragmentWeight(R.id.bottom_container, 0f);
-        }
-    }
-
     @Override
     public void onTouchEvent() {
-        setActionsVisible(false);
         if (currentStep == Step.START) {
             currentStep = Step.ZOOM1;
         }
-        //start timer to show again
-        handler.removeCallbacks(runnable);
-        handler.postDelayed(runnable, Constants.ACTIONS_DIALOG_OFF_TIMEOUT);
     }
 
     private enum Step {
