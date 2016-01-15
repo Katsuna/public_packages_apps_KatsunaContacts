d158f06a70698c4f973ee70aae75948d715f3515
KTakaf <ktakaf@gmail.com>
2016-01-15 15:39:37 +0200

    take image

--- app/src/main/java/gr/crystalogic/oldmen/ui/activities/CreateContactActivity.java
+++ app/src/main/java/gr/crystalogic/oldmen/ui/activities/CreateContactActivity.java
@@ -1,17 +1,25 @@
 package gr.crystalogic.oldmen.ui.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import gr.crystalogic.oldmen.R;
+import gr.crystalogic.oldmen.utils.ImageHelper;
 
 public class CreateContactActivity extends AppCompatActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView mPhoto;
     private EditText mName;
     private EditText mSurname;
     private EditText mTelephone;
@@ -25,9 +33,17 @@ public class CreateContactActivity extends AppCompatActivity {
     }
 
     private void initControls() {
+        mPhoto = (ImageView) findViewById(R.id.photo);
         mName = (EditText) findViewById(R.id.name);
         mSurname = (EditText) findViewById(R.id.surname);
         mTelephone = (EditText) findViewById(R.id.telephone);
+
+        mPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
         mName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -72,5 +88,24 @@ public class CreateContactActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Bitmap centerCropped = ImageHelper.centerCrop(imageBitmap);
+            Bitmap maskedBitmap = ImageHelper.getMaskedBitmap(getResources(), centerCropped, R.drawable.avatar);
+            //mPhoto.setImageBitmap(imageBitmap);
+            mPhoto.setImageBitmap(maskedBitmap);
+        }
     }
 }
--- app/src/main/java/gr/crystalogic/oldmen/utils/ImageHelper.java
+++ app/src/main/java/gr/crystalogic/oldmen/utils/ImageHelper.java
@@ -29,4 +29,31 @@ public class ImageHelper {
         mask.recycle();
         return bitmap;
     }
+
+    public static Bitmap centerCrop(Bitmap srcBmp) {
+        Bitmap dstBmp;
+
+        if (srcBmp.getWidth() >= srcBmp.getHeight()){
+
+            dstBmp = Bitmap.createBitmap(
+                    srcBmp,
+                    srcBmp.getWidth()/2 - srcBmp.getHeight()/2,
+                    0,
+                    srcBmp.getHeight(),
+                    srcBmp.getHeight()
+            );
+
+        }else{
+
+            dstBmp = Bitmap.createBitmap(
+                    srcBmp,
+                    0,
+                    srcBmp.getHeight()/2 - srcBmp.getWidth()/2,
+                    srcBmp.getWidth(),
+                    srcBmp.getWidth()
+            );
+        }
+
+        return dstBmp;
+    }
 }
