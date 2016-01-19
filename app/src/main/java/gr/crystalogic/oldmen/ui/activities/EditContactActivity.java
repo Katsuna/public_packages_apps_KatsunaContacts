61800a5bbec6afa6806375c534ff6bdcfb2148bf
KTakaf <ktakaf@gmail.com>
2016-01-19 14:08:56 +0200

    image changing

--- app/src/main/java/gr/crystalogic/oldmen/ui/activities/EditContactActivity.java
+++ app/src/main/java/gr/crystalogic/oldmen/ui/activities/EditContactActivity.java
@@ -1,7 +1,10 @@
 package gr.crystalogic.oldmen.ui.activities;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,10 +15,12 @@ import android.widget.ImageView;
 import gr.crystalogic.oldmen.R;
 import gr.crystalogic.oldmen.dao.ContactDao;
 import gr.crystalogic.oldmen.domain.Contact;
+import gr.crystalogic.oldmen.utils.ImageHelper;
 
 public class EditContactActivity extends AppCompatActivity {
 
-    private ImageView mPhoto;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
     private EditText mName;
     private EditText mSurname;
     private EditText mTelephone1;
@@ -23,6 +28,8 @@ public class EditContactActivity extends AppCompatActivity {
     private EditText mTelephone3;
     private EditText mEmail;
     private EditText mAddress;
+    private ImageView mPhoto;
+    private Bitmap mBitmap;
 
     private FloatingActionButton mEditContactFab;
 
@@ -31,7 +38,12 @@ public class EditContactActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_contact);
 
-        mPhoto = (ImageView) findViewById(R.id.photo);
+        initControls();
+        loadContact();
+        setupFab();
+    }
+
+    private void initControls() {
         mName = (EditText) findViewById(R.id.name);
         mSurname = (EditText) findViewById(R.id.surname);
         mTelephone1 = (EditText) findViewById(R.id.telephone1);
@@ -40,6 +52,16 @@ public class EditContactActivity extends AppCompatActivity {
         mEmail = (EditText) findViewById(R.id.email);
         mAddress = (EditText) findViewById(R.id.address);
 
+        mPhoto = (ImageView) findViewById(R.id.photo);
+        mPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void loadContact() {
         String contactId = getIntent().getStringExtra("contactId");
         ContactDao contactDao = new ContactDao(this);
 
@@ -55,10 +77,11 @@ public class EditContactActivity extends AppCompatActivity {
         mAddress.setText(contact.getAddress());
 
         if (contact.getPhoto() != null) {
-            mPhoto.setImageBitmap(contact.getPhoto());
-        }
 
-        setupFab();
+            Bitmap maskedBitmap = ImageHelper.getMaskedBitmap(getResources(), contact.getPhoto(), R.drawable.avatar);
+
+            mPhoto.setImageBitmap(maskedBitmap);
+        }
     }
 
     private void setupFab() {
@@ -70,6 +93,25 @@ public class EditContactActivity extends AppCompatActivity {
                 //
             }
         });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            Bitmap centeredBitmap = ImageHelper.centerCrop(bitmap);
+            mBitmap = centeredBitmap;
+            Bitmap maskedBitmap = ImageHelper.getMaskedBitmap(getResources(), centeredBitmap, R.drawable.avatar);
+            mPhoto.setImageBitmap(maskedBitmap);
+        }
     }
 
 }
