276cb74d6c812230e730ac57faa215606bcd5817
George G <kreach3r@users.noreply.github.com>
2016-08-27 17:41:42 +0300

    Build inline & dependencies

---
+++ Android.mk
@@ -0,0 +1,47 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
+
+LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
+LOCAL_RESOURCE_DIR += frameworks/support/v7/appcompat/res
+LOCAL_RESOURCE_DIR += frameworks/support/v7/recyclerview/res
+LOCAL_RESOURCE_DIR += frameworks/support/design/res
+
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
+LOCAL_STATIC_JAVA_LIBRARIES += ezvcard
+LOCAL_STATIC_JAVA_AAR_LIBRARIES += roundedimageview
+
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.recyclerview
+LOCAL_AAPT_FLAGS += --extra-packages android.support.design
+LOCAL_AAPT_FLAGS += --extra-packages ezvcard
+LOCAL_AAPT_FLAGS += --extra-packages picasso
+LOCAL_AAPT_FLAGS += --extra-packages jodatime
+LOCAL_AAPT_FLAGS += --extra-packages roundedimageview
+
+LOCAL_PACKAGE_NAME := KatsunaContacts
+LOCAL_CERTIFICATE := platform
+#LOCAL_PROGUARD_FLAG_FILES := app/proguard-rules.pro
+
+LOCAL_PROGUARD_ENABLED := disabled
+
+include packages/apps/KatsunaCommon/common.mk
+
+include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += ezvcard:app/libs/ezvcard-0.9.9.jar
+
+include $(BUILD_MULTI_PREBUILT)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
