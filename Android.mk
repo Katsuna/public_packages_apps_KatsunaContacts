7828430e7e7f319da376a64fd8441813018728b5
George G <kreach3r@users.noreply.github.com>
2016-10-12 12:16:21 +0200

    Android.mk: include picasso in the build

--- Android.mk
+++ Android.mk
@@ -21,7 +21,8 @@ LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
 LOCAL_STATIC_JAVA_LIBRARIES += ezvcard
 
-# This is to include the specified aar, DEFINED in KatsunaCommon's Android.mk! 
+# Include the specified aar/jar (s), DEFINED in KatsunaCommon's Android.mk!
+LOCAL_STATIC_JAVA_LIBRARIES += picasso
 LOCAL_STATIC_JAVA_AAR_LIBRARIES := roundedimageview
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
