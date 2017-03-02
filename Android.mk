7d3ef9c9bd8837f6d12df34baeb2286d98923917
George G <kreach3r@users.noreply.github.com>
2017-03-02 20:40:56 +0100

    Android.mk: New KatsunaCommon setup
    * Added a way to merge AndroidManifests (like gradle should do)
    * Removed some obsolete comments
    * Enabled resources from KatsunaCommon
    * Added new KatsunaDependencies include

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+KATSUNA_COMMON_PATH := $(ANDROID_BUILD_TOP)/frameworks/KatsunaCommon
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
@@ -6,6 +7,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
 
 LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
+LOCAL_FULL_LIBS_MANIFEST_FILES := $(KATSUNA_COMMON_PATH)/commons/src/main/AndroidManifest.xml
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
 LOCAL_RESOURCE_DIR += frameworks/support/v7/appcompat/res
 LOCAL_RESOURCE_DIR += frameworks/support/v7/recyclerview/res
@@ -26,22 +28,17 @@ LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
 LOCAL_STATIC_JAVA_LIBRARIES += ezvcard
 
-# Include the specified aar/jar (s), DEFINED in KatsunaCommon's Android.mk!
 LOCAL_STATIC_JAVA_LIBRARIES += picasso
 LOCAL_STATIC_JAVA_AAR_LIBRARIES := roundedimageview
-
 LOCAL_STATIC_JAVA_AAR_LIBRARIES += fabtransformation
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_FLAGS += --generate-dependencies
-# TODO: Don't know if actually needed
-LOCAL_AAPT_FLAGS += --extra-packages com.katsuna.common
+LOCAL_AAPT_FLAGS += --extra-packages com.katsuna.commons
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.recyclerview
 LOCAL_AAPT_FLAGS += --extra-packages android.support.design
-# This is to include the aar's RESOURCES into this app
-# Notice the full packagename
 LOCAL_AAPT_FLAGS += --extra-packages com.makeramen.roundedimageview
 LOCAL_AAPT_FLAGS += --extra-packages com.konifar.fab_transformation
 
@@ -63,14 +60,3 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/preferred-apps
 LOCAL_SRC_FILES := aosp/etc/$(LOCAL_MODULE)
 
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-
-# Define here, which extra jar/aar this app needs
-# These should NOT be included in KatsunaCommon
-# These should reside inside aosp/libs of this app
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := ezvcard:aosp/libs/ezvcard-0.9.9.jar
-
-include $(BUILD_MULTI_PREBUILT)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
