6c34d8ff204a597010e0529d56c8867ec7b01fc6
alkis <a.kafkis@gmail.com>
2018-07-23 20:46:39 +0300

    Crashlytics added.

--- app/src/main/java/com/katsuna/contacts/ContactsApplication.java
+++ app/src/main/java/com/katsuna/contacts/ContactsApplication.java
@@ -1,9 +1,6 @@
 package com.katsuna.contacts;
 
 import android.app.Application;
-import android.util.Log;
-
-import com.google.firebase.crash.FirebaseCrash;
 
 public class ContactsApplication extends Application {
 
@@ -12,13 +9,6 @@ public class ContactsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        try {
-            // disable firebase crash collection for debug
-            FirebaseCrash.setCrashCollectionEnabled(!BuildConfig.DEBUG);
-        } catch (Exception ex) {
-            Log.e(TAG, ex.getMessage());
-        }
     }
 
 }
